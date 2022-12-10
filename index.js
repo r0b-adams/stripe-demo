ac51c2a9c318b7e7aa20eeb70605b14367e8e6e6
Robert Adams <adamsiii.robert@gmail.com>
2022-12-09 20:03:51 -0800

    factor out helpers

--- index.js
+++ index.js
@@ -1,42 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
+const { mapItems } = require("./middleware");
 
-const SUCCESS_URL = path.join(process.env.SERVER_URL, "/success.html");
-const CANCEL_URL = path.join(process.env.SERVER_URL, "/cancel.html");
-
-// Initialize the app and create a port
 const app = express();
-const PORT = process.env.PORT || 3002;
-
-// Set up body parsing, static, and route middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-const storeItems = new Map([
-  [1, { price: 10000, name: "learn react today" }],
-  [2, { price: 20000, name: "learn css today" }],
-]);
-
-const mapItems = (req, res, next) => {
-  const items = req.body.items.map(item => {
-    const storeItem = storeItems.get(item.id);
-    return {
-      price_data: {
-        currency: "usd",
-        product_data: {
-          name: storeItem.name,
-        },
-        unit_amount: storeItem.price,
-      },
-      quantity: item.qty,
-    };
-  });
-  req.line_items = items;
-  next();
-};
 
 app.post("/create-checkout-session", mapItems, async (req, res) => {
   try {
@@ -54,5 +24,6 @@ app.post("/create-checkout-session", mapItems, async (req, res) => {
   }
 });
 
-// Start the server on the port
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.listen(process.env.SERVER_PORT, () =>
+  console.log(`listening on PORT ${process.env.SERVER_PORT}`)
+);
